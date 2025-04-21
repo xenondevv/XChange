55c6f9f6feb515d540adaaef09bf3e971f68b4fd
Devansh <devanshdevsinghcnet7@gmail.com>
2025-04-21 13:33:46 +0530

    code-readability_updated

--- App.js
+++ App.js
@@ -11,34 +11,34 @@ const msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
   for (currcode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currcode;
-    newOption.value = currcode;
+    let newOption = document.createElement("option"); //option element created
+    newOption.innerText = currcode; //text of the option
+    newOption.value = currcode; //value of the option
     if (select.name === "from" && currcode === "USD") {
-      newOption.selected = "selected";
+      newOption.selected = "selected"; //default selected option
     } else if (select.name === "to" && currcode === "INR") {
       newOption.selected = "selected";
     }
-    select.append(newOption);
+    select.append(newOption); //append the option to the select element
   }
 
   select.addEventListener("change", (evt) => {
-    updateFlag(evt.target);
+    updateFlag(evt.target); // update the flag when the select element changes
   });
 }
 
 const updateFlag = (element) => {
-  let currCode = element.value;
-  let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newSrc;
+  let currCode = element.value; // get the selected currency code 
+  let countryCode = countryList[currCode]; //get the country code from the countryList object
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`; // create the new image source URL
+  let img = element.parentElement.querySelector("img"); // get the image element
+  img.src = newSrc; // set the new image source
 };
 
-btn.addEventListener("click", async (evt) => {
+btn.addEventListener("click", async (evt) => { //
   evt.preventDefault();
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
+  let amount = document.querySelector(".amount input"); // get the amount input element
+  let amtVal = amount.value; // get the value in at varibale
   if (amtVal === "" || amtVal <= 0) {
     alert("Please enter a valid amount");
     amtVal = 1;
@@ -46,14 +46,14 @@ btn.addEventListener("click", async (evt) => {
   }
   console.log(fromCurr.value, toCurr.value);
   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.min.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
+  let response = await fetch(URL); // fetch the data from the API
+  let data = await response.json(); // convert the response to JSON
   console.log(data);
-  let from = fromCurr.value.toLowerCase();
+  let from = fromCurr.value.toLowerCase(); 
   let to = toCurr.value.toLowerCase();
   console.log(from, to);
-  let rate = data[from][to];
+  let rate = data[from][to]; //get the exchange rate from the data
   let finalAmount = amtVal * rate;
   console.log(finalAmount);
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`; // display the result
 });
